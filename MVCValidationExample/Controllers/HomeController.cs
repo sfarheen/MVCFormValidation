7823b55512b9fb4857903d04ba0a342c1406e52c
Farheen Sultana <farheensultana999@hotmail.com>
2016-10-25 11:35:52 -0400

    form validations

---
+++ MVCValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using MVCValidationExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCValidationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(Register reg)
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(reg);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        
+        //
+        // GET: /Home/
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(Register registerDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Success = "User Successfully Registered !!";
+                return RedirectToAction("about", registerDetails);
+            }
+            else
+            {
+                return View("Signup", registerDetails);
+            }
+        }
+    }
+}
+
+
---
+++ MVCValidationExample/Models/Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCValidationExample.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "FirstName is Required")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(20, 40, ErrorMessage = "Age must be between 20 and 40")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Not a valid format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        [StringLength(10, ErrorMessage = "Password must be less than 10 letters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword is Required")]
+        [DataType(DataType.Password)]
+        [StringLength(10, ErrorMessage = "Password must be less than 10 letters")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
